d12b2965c8750215a834bc86c9c0b30f7f69377e
Lucian <lucianqin.dev@gmail.com>
2022-04-04 21:15:25 +1000

    go back page

---
+++ cypress/integration/Test4.js
@@ -0,0 +1,20 @@
+/// < reference types='Cypress' />
+
+describe("fourth test", () => {
+  it("should first", () => {
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice");
+
+    cy.get("#alertbtn").click();
+    cy.get('[value="Confirm"]').click();
+    // on: fiire an event and (things needs to compare)
+    cy.on("window.alert", (str) => {
+      expect(str).to.equal("Hello, Are you sure you want to confirm?");
+    });
+
+    cy.get("#opentab").invoke("removeAttr", "target").click();
+
+    cy.url().should("include", "academy");
+
+    cy.go("back");
+  });
+});
